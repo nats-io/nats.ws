7ab87a61b4c0be7e49598516257c1910d869bb61
Alberto Ricart <alberto@synadia.com>
2022-04-26 16:32:43 -0500

    [UPDATE] nats-server to 2.8.1 [UPDATE] deno to 1.21.0 and std to 136 [CHANGE] fixed tests to reflect new behaviour on nbc where subscription/publish permission errors don't close the client.

--- src/nats-base-client.ts
+++ src/nats-base-client.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2021 The NATS Authors
+ * Copyright 2020-2022 The NATS Authors
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,4 +13,4 @@
  * limitations under the License.
  */
 // this import here to drive the build system
-export * from "https://raw.githubusercontent.com/nats-io/nats.deno/bump-deno-1_21/nats-base-client/internal_mod.ts";
+export * from "https://raw.githubusercontent.com/nats-io/nats.deno/main/nats-base-client/internal_mod.ts";
