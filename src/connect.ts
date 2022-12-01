d9b0a0365cc519f38adec11a3d672a0cf6bc9d3c
Alberto Ricart <alberto@synadia.com>
2022-12-01 16:59:17 -0400

    [BUMP] version 1.10.2 (#176)
    [BUMP] nbc to 1.9.3

--- src/connect.ts
+++ src/connect.ts
@@ -19,7 +19,7 @@ import {
   setTransportFactory,
   Transport,
   TransportFactory,
-} from "https://raw.githubusercontent.com/nats-io/nats.deno/v1.9.2/nats-base-client/internal_mod.ts";
+} from "https://raw.githubusercontent.com/nats-io/nats.deno/v1.9.3/nats-base-client/internal_mod.ts";
 
 import { WsTransport } from "./ws_transport.ts";
 
