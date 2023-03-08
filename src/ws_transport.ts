4bdb28e45f2816025d3df236656ac202372bc937
Alberto Ricart <alberto@synadia.com>
2023-03-08 14:49:13 -0600

    [BUMP] nbc to v1.13.0, and ci deps

--- src/ws_transport.ts
+++ src/ws_transport.ts
@@ -19,7 +19,7 @@ import type {
   Server,
   ServerInfo,
   Transport,
-} from "https://raw.githubusercontent.com/nats-io/nats.deno/v1.12.1/nats-base-client/internal_mod.ts";
+} from "https://raw.githubusercontent.com/nats-io/nats.deno/v1.13.0/nats-base-client/internal_mod.ts";
 import {
   checkOptions,
   DataBuffer,
@@ -30,9 +30,9 @@ import {
   INFO,
   NatsError,
   render,
-} from "https://raw.githubusercontent.com/nats-io/nats.deno/v1.12.1/nats-base-client/internal_mod.ts";
+} from "https://raw.githubusercontent.com/nats-io/nats.deno/v1.13.0/nats-base-client/internal_mod.ts";
 
-const VERSION = "1.13.1";
+const VERSION = "1.14.0";
 const LANG = "nats.ws";
 
 export type WsSocketFactory = (u: string, opts: ConnectionOptions) => Promise<{
