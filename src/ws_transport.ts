a4e457d8ebb02e6eb6bef855b27dc3da5a2e8e74
Alberto Ricart <alberto@synadia.com>
2023-08-16 13:37:02 -0500

    [BUMP] ci dependencies and lib version

--- src/ws_transport.ts
+++ src/ws_transport.ts
@@ -19,7 +19,7 @@ import type {
   Server,
   ServerInfo,
   Transport,
-} from "https://raw.githubusercontent.com/nats-io/nats.deno/v1.15.1/nats-base-client/internal_mod.ts";
+} from "https://raw.githubusercontent.com/nats-io/nats.deno/dev/nats-base-client/internal_mod.ts";
 import {
   checkOptions,
   DataBuffer,
@@ -30,9 +30,9 @@ import {
   INFO,
   NatsError,
   render,
-} from "https://raw.githubusercontent.com/nats-io/nats.deno/v1.15.1/nats-base-client/internal_mod.ts";
+} from "https://raw.githubusercontent.com/nats-io/nats.deno/dev/nats-base-client/internal_mod.ts";
 
-const VERSION = "1.16.1";
+const VERSION = "1.17.0";
 const LANG = "nats.ws";
 
 export type WsSocketFactory = (u: string, opts: ConnectionOptions) => Promise<{
