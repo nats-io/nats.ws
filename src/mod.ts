71d1db4ac3ffacf8df62bd313147149c90925af1
Alberto Ricart <alberto@synadia.com>
2024-07-24 14:59:43 -0500

    [BUMP] nbc to 1.28.1, client version, ci deps (#251)

--- src/mod.ts
+++ src/mod.ts
@@ -12,6 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export * from "https://raw.githubusercontent.com/nats-io/nats.deno/v1.28.0/nats-base-client/mod.ts";
-export * from "https://raw.githubusercontent.com/nats-io/nats.deno/v1.28.0/jetstream/mod.ts";
+export * from "https://raw.githubusercontent.com/nats-io/nats.deno/v1.28.1/nats-base-client/mod.ts";
+export * from "https://raw.githubusercontent.com/nats-io/nats.deno/v1.28.1/jetstream/mod.ts";
 export { connect } from "./connect.ts";
